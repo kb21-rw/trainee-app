fcec751859bed0e8cde1da6794b4defd821a37f1
Nestor <Nestor Ngabonziza>
2023-09-18 10:44:01 +0200

    feat: adding the register

---
+++ backend/src/utils/helpers.ts
@@ -0,0 +1,61 @@
+import nodeMailer from "nodemailer"
+import dotenv from "dotenv"
+
+dotenv.config()
+const user = process.env.NODEMAIL_EMAIL;
+const pass = process.env.NODEMAIL_PASSWORD
+export const generateRandomPassword = (length: number)=> {
+    const charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%&";
+    let randomString = "";
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        randomString += charset[randomIndex];
+    }
+
+    return randomString;
+}
+  export const sendEmail = async (name:string, email:string, subject:string, message:string)=> {
+   const transporter = nodeMailer.createTransport({
+    service: "gmail",
+    auth:{
+       user,
+       pass
+    }
+   })
+   const info:any = await transporter.sendMail({
+    from: `${name} <${email}>`,
+    to:email,
+    subject,
+    html:message
+   })
+   console.log("Message sent: "+info.messageId)
+}
+
+export const generateMessage = (name:string,email:string,role:string,password:string)=>{
+    const html=`<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Welcome as ${role}</title>
+    </head>
+    <body>
+        <div style="font-family: Arial, sans-serif; margin: 20px;">
+            <h1>Hello ${name},</h1>
+            <p>You are registered as ${role.toLowerCase()}</p>
+            ${password?`<h3>Your credentials</h3>
+            <div>
+            <span>Your email: </span>
+            <span>${email}</span>
+            </div>
+            <div>
+            <span>Your password:   </span>
+            <span>${password}</span>
+            </div>`:""}
+            <p>Sincerely,<br> The gym</p>
+        </div>
+    </body>
+    </html>
+    `
+    return html
+}
+
