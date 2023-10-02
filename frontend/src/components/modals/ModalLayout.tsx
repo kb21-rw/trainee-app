16bab2acbe9f14e4d8654cdae5bdfbf0b361ddf5
estherniyo <estherniyonkuru123@gmail.com>
2023-10-02 16:09:52 +0200

    feat: add scroll inside popup layout and stop scroll outside the layout

--- frontend/src/components/modals/ModalLayout.tsx
+++ frontend/src/components/modals/ModalLayout.tsx
@@ -16,7 +16,7 @@ const ModalLayout = ({
   }, [closePopup]);
   return (
     <div className="absolute inset-0 h-screen w-full flex items-center justify-center z-50 bg-secondary-dark bg-opacity-40">
-      <div className=" bg-white rounded-3xl flex flex-col overflow-scroll w-1/3 h-3/5 px-8 pt-9 gap-8 pb-8">
+      <div className=" bg-white rounded-3xl flex flex-col overflow-y-auto w-1/3 h-3/5 px-8 pt-9 gap-8 pb-8">
         <div className="flex">
           <h1 className="flex-1 flex items-center justify-center font-bold text-[40px] leading-[50px] ">{title}</h1>
           <button className="bg-[#F8F6FA] h-9 w-9 flex items-center justify-center self-start rounded-s" onClick={()=>closePopup()}>
