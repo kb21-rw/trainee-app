b32453ccb3a01644da895c18cf91ef8c8baee935
Nestor <Nestor Ngabonziza>
2023-10-02 13:02:34 +0200

    feat: adding sparse email to allow unique and optional in email field

--- backend/dist/validations/authValidation.js
+++ backend/dist/validations/authValidation.js
@@ -6,9 +6,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.coachAssignSchema = exports.ProfileSchema = exports.loginSchema = exports.registerSchema = void 0;
 const joi_1 = __importDefault(require("joi"));
 exports.registerSchema = joi_1.default.object({
-    name: joi_1.default.string().alphanum().min(3).max(30).required(),
-    email: joi_1.default.string().email().required(),
-    role: joi_1.default.string().valid("ADMIN", "COACH", "TRAINEE").required()
+    name: joi_1.default.string().min(3).max(30).required(),
+    email: joi_1.default.string().email(),
+    role: joi_1.default.string().valid("ADMIN", "COACH", "TRAINEE").required(),
+    coach: joi_1.default.string(),
 });
 exports.loginSchema = joi_1.default.object({
     email: joi_1.default.string().email().required(),
