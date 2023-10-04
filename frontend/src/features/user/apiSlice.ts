52d933f122be6c201c09e2aff5f0bff4b5d759eb
Nestor <Nestor Ngabonziza>
2023-10-04 17:00:39 +0200

    feat: search and sort coaches

--- frontend/src/features/user/apiSlice.ts
+++ frontend/src/features/user/apiSlice.ts
@@ -1,139 +1,168 @@
-import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react"
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import Cookies from "universal-cookie";
 
 const api_url = import.meta.env.VITE_API_URL;
 
-const cookies = new Cookies()
-const jwt = cookies.get("jwt")
+const cookies = new Cookies();
+const jwt = cookies.get("jwt");
 
-export  const usersApi:any = createApi({
-    reducerPath: "usersApi",
-    baseQuery: fetchBaseQuery({baseUrl: api_url}),
-    tagTypes: ["coaches", "trainees", "myTrainees", "profile"],
-    endpoints: (builder)=>({
-        getAllTrainees: builder.query({
-            query: (jwt) => ({
-                url: '/auth/trainees',
-                method: 'GET',
-                headers: {
-                    Authorization: `Bearer ${jwt}`,
-                },
-            }),
-            providesTags: ["trainees"]
-        }),
-        getMyTrainees: builder.query({
-            query: (jwt) => ({
-                url: '/auth/my-trainees',
-                method: 'GET',
-                headers: {
-                    Authorization: `Bearer ${jwt}`,
-                },
-            }),
-            providesTags: ["myTrainees"]
-        }),
-        getAllCoaches: builder.query({
-            query: (jwt) => ({
-                url: '/auth/coaches',
-                method: 'GET',
-                headers: {
-                    Authorization: `Bearer ${jwt}`,
-                },
-            }),
-            providesTags: ["coaches"]
-        }),
-        createCoach: builder.mutation({
-            query: (args) => {
-                const {jwt, body} = args
-                return ({
-                url: '/auth/register',
-                method: 'POST',
-                headers: {
-                    Authorization: `Bearer ${jwt}`,
-                },
-                body: {...body}
-            })}, 
-            invalidatesTags: ["coaches"]
-        }),
-        createTrainee: builder.mutation({
-            query: (args) => {
-                const {jwt, body} = args
-                return ({
-                url: '/auth/register',
-                method: 'POST',
-                headers: {
-                    Authorization: `Bearer ${jwt}`,
-                },
-                body: {...body}
-            })}, 
-            invalidatesTags: ["trainees"]
-        }),
-        deleteCoach: builder.mutation({
-            query: (args) => {
-                const {jwt, id} = args
-                return ({
-                url: `/auth/users/${id}`,
-                method: 'DELETE',
-                headers: {
-                    Authorization: `Bearer ${jwt}`,
-                },
-            })}, 
-            invalidatesTags: ["coaches",]
-        }),
-        deleteTrainee: builder.mutation({
-            query: (args) => {
-                const {jwt, id} = args
-                return ({
-                url: `/auth/users/${id}`,
-                method: 'DELETE',
-                headers: {
-                    Authorization: `Bearer ${jwt}`,
-                },
-            })}, 
-            invalidatesTags: ["trainees",]
-        }),
+export const usersApi: any = createApi({
+  reducerPath: "usersApi",
+  baseQuery: fetchBaseQuery({ baseUrl: api_url }),
+  tagTypes: ["coaches", "trainees", "myTrainees", "profile"],
+  endpoints: (builder) => ({
+    getAllTrainees: builder.query({
+      query: (args) => {
+        const { jwt, query } = args;
+        return {
+          url: `/auth/trainees${query}`,
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${jwt}`,
+          },
+        };
+      },
+      providesTags: ["trainees"],
+    }),
+    getMyTrainees: builder.query({
+      query: (args) => {
+        const { jwt, query } = args;
+        return {
+          url: "/auth/my-trainees",
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${jwt}`,
+          },
+        };
+      },
 
-        login: builder.mutation({
-            query: (body) => {
-                return ({
-                url: '/auth/login',
-                method: 'POST',
-                body: {...body}
-            })}, 
-            invalidatesTags: ["profile"]
-        }),
-        resetPassword: builder.mutation({
-            query: (body) => {
-                return ({
-                url: '/auth/reset-password',
-                method: 'POST',
-                body
-            })}, 
-        }),
-        getProfile: builder.query({
-            query: (jwt) => ({
-                url: '/auth/profile',
-                method: 'GET',
-                headers: {
-                    Authorization: `Bearer ${jwt}`,
-                },
-            }),
-            providesTags: ["profile"]
-        }),
-        updateProfile: builder.mutation({
-            query: (args) => {
-                const {jwt, profileData} = args
-                console.log({args})
-                return ({
-                url: '/auth/profile',
-                method: 'PUT',
-                headers: {
-                    Authorization: `Bearer ${jwt}`,
-                },
-                body: {...profileData}
-            })}, 
-            invalidatesTags: ["profile"]
-        }),
-    })
-})
+      providesTags: ["myTrainees"],
+    }),
+    getAllCoaches: builder.query({
+      query: (args) => {
+        const { jwt, query } = args;
+        return {
+          url: `/auth/coaches${query}`,
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${jwt}`,
+          },
+        };
+      },
+      providesTags: ["coaches"],
+    }),
+    createCoach: builder.mutation({
+      query: (args) => {
+        const { jwt, body } = args;
+        return {
+          url: "/auth/register",
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${jwt}`,
+          },
+          body: { ...body },
+        };
+      },
+      invalidatesTags: ["coaches"],
+    }),
+    createTrainee: builder.mutation({
+      query: (args) => {
+        const { jwt, body } = args;
+        return {
+          url: "/auth/register",
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${jwt}`,
+          },
+          body: { ...body },
+        };
+      },
+      invalidatesTags: ["trainees"],
+    }),
+    deleteCoach: builder.mutation({
+      query: (args) => {
+        const { jwt, id } = args;
+        return {
+          url: `/auth/users/${id}`,
+          method: "DELETE",
+          headers: {
+            Authorization: `Bearer ${jwt}`,
+          },
+        };
+      },
+      invalidatesTags: ["coaches"],
+    }),
+    deleteTrainee: builder.mutation({
+      query: (args) => {
+        const { jwt, id } = args;
+        return {
+          url: `/auth/users/${id}`,
+          method: "DELETE",
+          headers: {
+            Authorization: `Bearer ${jwt}`,
+          },
+        };
+      },
+      invalidatesTags: ["trainees"],
+    }),
 
-export const { useGetAllTraineesQuery, useGetAllCoachesQuery, useCreateCoachMutation, useCreateTraineeMutation, useLoginMutation, useGetProfileQuery, useUpdateProfileMutation, useResetPasswordMutation, useDeleteCoachMutation, useDeleteTraineeMutation, useGetMyTraineesQuery } = usersApi
+    login: builder.mutation({
+      query: (body) => {
+        return {
+          url: "/auth/login",
+          method: "POST",
+          body: { ...body },
+        };
+      },
+      invalidatesTags: ["profile"],
+    }),
+    resetPassword: builder.mutation({
+      query: (body) => {
+        return {
+          url: "/auth/reset-password",
+          method: "POST",
+          body,
+        };
+      },
+    }),
+    getProfile: builder.query({
+      query: (jwt) => ({
+        url: "/auth/profile",
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${jwt}`,
+        },
+      }),
+      providesTags: ["profile"],
+    }),
+    updateProfile: builder.mutation({
+      query: (args) => {
+        const { jwt, profileData } = args;
+        console.log({ args });
+        return {
+          url: "/auth/profile",
+          method: "PUT",
+          headers: {
+            Authorization: `Bearer ${jwt}`,
+          },
+          body: { ...profileData },
+        };
+      },
+      invalidatesTags: ["profile"],
+    }),
+  }),
+});
+
+export const {
+  useGetAllTraineesQuery,
+  useGetAllCoachesQuery,
+  useCreateCoachMutation,
+  useCreateTraineeMutation,
+  useLoginMutation,
+  useGetProfileQuery,
+  useUpdateProfileMutation,
+  useResetPasswordMutation,
+  useDeleteCoachMutation,
+  useDeleteTraineeMutation,
+  useGetMyTraineesQuery,
+} = usersApi;
--- frontend/src/pages/User/CoachesInfo.tsx
+++ frontend/src/pages/User/CoachesInfo.tsx
@@ -1,98 +1,160 @@
-import React, { useState } from 'react'
-import Button from '../../components/ui/Button'
-import Plus from "../../assets/Plus"
+import React, { useEffect, useState, useRef, InputHTMLAttributes } from "react";
+import Button from "../../components/ui/Button";
+import Plus from "../../assets/Plus";
 import Sort from "../../assets/Sort";
-import Edit from '../../assets/Edit'
-import Delete from '../../assets/Delete'
-import Cookies from "universal-cookie"
-import Loader from '../../components/ui/Loader'
-import { useDeleteCoachMutation, useGetAllCoachesQuery } from '../../features/user/apiSlice';
-import AddingCoachModal from '../../components/modals/AddingCoachModal';
-
+import Edit from "../../assets/Edit";
+import Delete from "../../assets/Delete";
+import Cookies from "universal-cookie";
+import Loader from "../../components/ui/Loader";
+import {
+  useDeleteCoachMutation,
+  useGetAllCoachesQuery,
+} from "../../features/user/apiSlice";
+import AddingCoachModal from "../../components/modals/AddingCoachModal";
 
 const CoachesInfo = () => {
-  const cookies = new Cookies()
-  const jwt = cookies.get("jwt")
-  const coachesData = useGetAllCoachesQuery(jwt)
-  const [openPopup, setOpenPopup] = useState(false)
-  const [deleteCoach, {isError, isLoading, error}] = useDeleteCoachMutation()
-  const handleDeleteCoach = (id:string) =>{
-   const result = deleteCoach({jwt, id})
-   console.log({result})
-  }
+  const cookies = new Cookies();
+  const jwt = cookies.get("jwt");
+  const searchRef = useRef<any>(null);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [sortBy, setSortBy] = useState("entry");
+  const [usersPerPage, setUsersPerPage] = useState("10");
+  const [query, setQuery] = useState("");
+  const coachesData = useGetAllCoachesQuery({ jwt, query });
+  const [openPopup, setOpenPopup] = useState(false);
+  const [deleteCoach, { isError, isLoading, error }] = useDeleteCoachMutation();
+  const handleDeleteCoach = (id: string) => {
+    const result = deleteCoach({ jwt, id });
+  };
+  const [searchString, setSearchString] = useState("");
+  const onSubmitSearch = () => {
+    setSearchQuery(searchString);
+  };
+  const handleKeyPress = (event: any) => {
+    if (event.key === "Enter") {
+      setSearchQuery(searchString);
+    }
+  };
+  useEffect(() => {
+    setQuery(
+      `?searchString=${searchQuery}&sortBy=${sortBy}&coachesPerPage=${usersPerPage}`
+    );
+  }, [searchQuery, sortBy, usersPerPage]);
   return (
     <div>
-    <div className='py-8'>
-        <div className='flex justify-end items-center my-6'>
-        
-        <Button clickHandler={()=>setOpenPopup(!openPopup)} variant='small'>
-            <Plus/>
+      <div className="py-8">
+        <div className="flex justify-end items-center my-6">
+          <Button clickHandler={() => setOpenPopup(!openPopup)} variant="small">
+            <Plus />
             <span>Add coach</span>
-            </Button>
-        </div><div className="flex items-center justify-between gap-16">
-      <div className="flex w-full  items-center max-w-xl px-1 py-1 h-[58px] border border-[#DBD5E0] rounded-xl">
-        <input
-          className="px-2 flex-1 outline-none border-none h-full"
-          placeholder="Enter name"
-          name="search"
-        />
-        <Button variant="small">Search</Button>
-      </div>
-      <div className="flex gap-4 items-center">
-      <label className="flex gap-6 items-center">
-      <div className="flex gap-2 items-center">
-          <Sort />
-          <span className="text-base font-normal text-[#5B576A] whitespace-nowrap">
-            Sort coaches by:
-          </span>
+          </Button>
         </div>
-        <select name="sort" className="forms-select outline-none bg-white gap-32 w-[72px] block py-2 ">
-          <option selected>Name</option>
-          <option>Coach</option>
-        </select>
-       
-      </label>
-        
-      <label className="flex gap-6 items-center">
-          <span className="text-base font-normal text-[#5B576A] whitespace-nowrap">
-            Coaches per page:
-          </span>
-        <select name="coachePerPage" className="forms-select outline-none bg-white gap-32 w-12 block py-2 ">
-          <option selected>20</option>
-          <option>30</option>
-          <option>40</option>
-          <option>50</option> 
-        </select>
-      </label> 
-      </div>
-    </div>
-            <table className='w-full my-8 table-auto'>
-        <thead className='bg-[#0077B6] bg-opacity-20 h-20'>
-            <tr className='w-full'>
-                <td className='rounded-l-xl pl-12 font-semibold' >No</td>
-                <td className='pl-12 font-semibold' >Name</td>
-                <td className='pl-12 font-semibold'>Email</td>
-                <td className='pl-12 font-semibold' >Role</td>
-                <td className="rounded-r-xl pl-12 font-semibold" >Action</td>
-                
-            </tr>
-        </thead>
-          {coachesData.status==="pending"?<div className='flex w-screen items-center justify-center h-[50vh]'><Loader/></div>:
-        <tbody className='w-full'>
-         { coachesData.data?.map((coach:any,index:number)=><tr key={coach._id} className='border-b border-black h-[100px] w-full'>
-                <td className='text-base font-medium pl-12' >{index+1}</td>
-                <td className='text-base font-medium pl-12' >{coach?.name}</td>
-                <td className='text-base font-medium pl-12' >{coach?.email}</td>
-                <td className='text-base font-medium pl-12' >{coach?.role}</td>
-                <td className='text-base font-medium pl-12' ><div className='flex items-center gap-4 w-full h-full'><button><Edit/></button> <button onClick={()=>handleDeleteCoach(coach._id)}><Delete/></button></div></td>
-            </tr>)}
-        </tbody>}
-    </table>
-            
-    </div>
-    {openPopup && <AddingCoachModal jwt={jwt} closePopup={()=>setOpenPopup(false)}/>}
-    </div>
-  )
-}
+        <div className="flex items-center justify-between gap-16">
+          <div className="flex w-full  items-center max-w-xl px-1 py-1 h-[58px] border border-[#DBD5E0] rounded-xl">
+            <input
+              className="px-2 flex-1 outline-none border-none h-full"
+              placeholder="Enter name"
+              name="search"
+              value={searchString}
+              onChange={(event) => setSearchString(event.target.value)}
+              ref={searchRef}
+              onKeyDown={handleKeyPress}
+            />
+            <Button clickHandler={onSubmitSearch} variant="small">
+              Search
+            </Button>
+          </div>
+          <div className="flex gap-4 items-center">
+            <label className="flex gap-6 items-center">
+              <div className="flex gap-2 items-center">
+                <Sort />
+                <span className="text-base font-normal text-[#5B576A] whitespace-nowrap">
+                  Sort coaches by:
+                </span>
+              </div>
+              <select
+                value={sortBy}
+                onChange={(event) => setSortBy(event.target.value)}
+                name="sort"
+                className="forms-select outline-none bg-white gap-32 w-[72px] block py-2 "
+              >
+                <option value="all" selected>
+                  Entry
+                </option>
+                <option value="name">Name</option>
+                <option value="email">Email</option>
+              </select>
+            </label>
 
-export default CoachesInfo
+            <label className="flex gap-6 items-center">
+              <span className="text-base font-normal text-[#5B576A] whitespace-nowrap">
+                Coaches per page:
+              </span>
+              <select
+                value={usersPerPage}
+                onChange={(event) => setUsersPerPage(event.target.value)}
+                name="coachePerPage"
+                className="forms-select outline-none bg-white gap-32 w-12 block py-2 "
+              >
+                <option value="10" selected>
+                  10
+                </option>
+                <option value="20">20</option>
+                <option value="30">30</option>
+                <option value="40">40</option>
+                <option value="50">50</option>
+              </select>
+            </label>
+          </div>
+        </div>
+        <table className="w-full my-8 table-auto">
+          <thead className="bg-[#0077B6] bg-opacity-20 h-20">
+            <tr className="w-full">
+              <td className="rounded-l-xl pl-12 font-semibold">No</td>
+              <td className="pl-12 font-semibold">Name</td>
+              <td className="pl-12 font-semibold">Email</td>
+              <td className="pl-12 font-semibold">Role</td>
+              <td className="rounded-r-xl pl-12 font-semibold">Action</td>
+            </tr>
+          </thead>
+          {coachesData.status === "pending" ? (
+            <div className="flex w-screen items-center justify-center h-[50vh]">
+              <Loader />
+            </div>
+          ) : (
+            <tbody className="w-full">
+              {coachesData.data?.map((coach: any, index: number) => (
+                <tr
+                  key={coach._id}
+                  className="border-b border-black h-[100px] w-full"
+                >
+                  <td className="text-base font-medium pl-12">{index + 1}</td>
+                  <td className="text-base font-medium pl-12">{coach?.name}</td>
+                  <td className="text-base font-medium pl-12">
+                    {coach?.email}
+                  </td>
+                  <td className="text-base font-medium pl-12">{coach?.role}</td>
+                  <td className="text-base font-medium pl-12">
+                    <div className="flex items-center gap-4 w-full h-full">
+                      <button>
+                        <Edit />
+                      </button>{" "}
+                      <button onClick={() => handleDeleteCoach(coach._id)}>
+                        <Delete />
+                      </button>
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          )}
+        </table>
+      </div>
+      {openPopup && (
+        <AddingCoachModal jwt={jwt} closePopup={() => setOpenPopup(false)} />
+      )}
+    </div>
+  );
+};
+
+export default CoachesInfo;
