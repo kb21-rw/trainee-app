fe788460048410f7efa92ee5df2df78f95baca7b
octan-6 <ntirandth@gmail.com>
2023-10-10 16:24:00 +0200

    removing assign-coach routes and change put to patch

--- backend/src/routes/userRoute.ts
+++ backend/src/routes/userRoute.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getUserProfile,assignCoach, get_coaches, get_trainees, updateUserProfile, deleteUser, editUser, get_users, editTrainee, get_my_trainees } from "../controllers/auth.controller";
+import { getUserProfile, get_coaches, get_trainees, updateUserProfile, deleteUser, editUser, get_users, editTrainee, get_my_trainees } from "../controllers/auth.controller";
 import { verifyJWT } from "../middlewares/authenticate";
 
 
@@ -9,11 +9,10 @@ router.get("/trainees",verifyJWT, get_trainees)
 router.get("/my-trainees",verifyJWT, get_my_trainees)
 router.get("/coaches",verifyJWT, get_coaches)
 router.get("/all",verifyJWT, get_users)
-router.patch("/assign-coach/:id",verifyJWT, assignCoach)
-router.put("/edit-coach-or-admin/:id", verifyJWT, editUser)
-router.put("/edit-trainee/:id", verifyJWT, editTrainee)
+router.patch("/edit-coach-or-admin/:id", verifyJWT, editUser)
+router.patch("/edit-trainee/:id", verifyJWT, editTrainee)
 router.get("/my-profile", verifyJWT, getUserProfile);
-router.put("/my-profile", verifyJWT, updateUserProfile); 
+router.patch("/my-profile", verifyJWT, updateUserProfile); 
 router.delete('/:userId', deleteUser);
 
 export default router
