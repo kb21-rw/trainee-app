46c85355c021a3ab7bd546230c7b3f30e0d4a087
Thierry N <thierryn@MacBook-Pro-von-Thierry.local>
2023-10-20 12:19:34 +0200

    fixing error page

--- frontend/src/components/Error.tsx
+++ frontend/src/components/Error.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { useRouteError } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+type ErrorType = {
+  message?: string
+}
 
 const Error = () => {
-  return <div>Error happened</div>;
+  const error = useRouteError() as ErrorType
+  return (
+    <div>
+      <h1>Error</h1>
+      <p>{error?.message}</p>
+      <Link to="/">Back to home page</Link>
+    </div>
+  )
+  
 };
 
 export default Error;
