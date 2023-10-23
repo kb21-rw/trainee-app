7468d45588e8cf728ccbafbada320a728d378513
rithaven <veneruth4@gmail.com>
2023-10-23 10:34:10 +0200

    feat: Adding an empty line to delimit the routing part from other sections

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -19,6 +19,7 @@ mongoose.connection.once("open", () => {
   });
 });
 app.use(cors({ origin: "*", credentials: true }));
+
 app.use(cookieParser());
 app.use(express.json());
 app.use("/auth", authRoute);
