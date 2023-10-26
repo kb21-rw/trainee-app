42e9d2037117a6eb86f11000b92e5bda9e6b6b3b
Thierry N <thierryn@MacBook-Pro-von-Thierry.local>
2023-10-26 16:40:25 +0200

    fix: make options required when the type of the questions is a dropdown

--- backend/src/validations/questionValidation.ts
+++ backend/src/validations/questionValidation.ts
@@ -1,8 +1,13 @@
 import Joi from "joi";
 
 export const createQuestionValidation = Joi.object({
-    title: Joi.string().min(3).max(100).required(),
-    type: Joi.string().valid("text", "dropdown").required(),
-    options: Joi.array().items(Joi.string())
-  });
-  
+  title: Joi.string().min(3).max(100).required(),
+  type: Joi.string().valid("text", "dropdown").required(),
+  options: Joi.array()
+    .items(Joi.string())
+    .when("type", {
+      is: "dropdown",
+      then: Joi.array().items(Joi.string()).min(1).required(),
+      otherwise: Joi.array().items(Joi.string()).optional(),
+    }),
+});
