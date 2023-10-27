ce3276f3d3a826ab333103b1d14c8d7ac6b24588
Nestor Ngabonziza <ngabonest@gmail.com>
2023-10-27 11:19:35 +0200

    refactor: add the space in the formvalidations

--- backend/src/validations/formValidation.ts
+++ backend/src/validations/formValidation.ts
@@ -4,6 +4,7 @@ export const createFormValidation = Joi.object({
   title: Joi.string().min(3).max(100).required(),
   description: Joi.string().min(3).max(100),
 });
+
 export const editFormValidation = Joi.object({
   title: Joi.string().min(3).max(100),
   description: Joi.string().min(3).max(100),
