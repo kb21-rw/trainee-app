ed175fde19857ffb372cb29ee36261b785ec2c3c
Thierry N <thierryn@MacBook-Pro-von-Thierry.local>
2023-10-30 07:24:11 +0200

    feat: building endpoints for delete and update a question

--- backend/src/controllers/questionController.ts
+++ backend/src/controllers/questionController.ts
@@ -33,3 +33,52 @@ export const createQuestion = async (req: Request, res: Response) => {
     return res.status(400).json({ error });
   }
 };
+
+export const updateQuestion = async (req: Request, res: Response) => {
+  try {
+    const { questionId } = req.params;
+    const validationResult: ValidationResult<CreateQuestionType> =
+      createQuestionValidation.validate(req.body);
+    if (validationResult.error) {
+      return res.status(400).json({ message: validationResult.error.message });
+    }
+
+    const { title, type, options }: CreateQuestionType = req.body;
+    const question = await Question.findById(questionId);
+    if (!question) {
+      return res.status(404).json({ message: "Question is not found" });
+    }
+
+    if (title) {
+      question.title = title;
+    }
+
+    if (type) {
+      question.type = type;
+    }
+
+    if (options) {
+      question.options = options;
+    }
+
+    await question.save();
+
+    return res.status(200).json(question);
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
+};
+
+export const deleteQuestion = async (req: Request, res: Response) => {
+  try {
+    const { questionId } = req.params;
+    const question = await Question.findByIdAndDelete(questionId);
+    if (!question) {
+      return res.status(404).send("Question is not found");
+    }
+
+    return res.status(200).send("Question deleted successfully");
+  } catch (error) {
+    return res.status(500).send("Internal Server Error");
+  }
+};
