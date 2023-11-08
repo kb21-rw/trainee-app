8be2aec61c8a187cd42a0fab0e77811fef7cfc40
Thierry N <thierryn@MacBook-Pro-von-Thierry.local>
2023-11-08 19:35:28 +0200

    remove trycatch

--- backend/src/utils/tryCatch.ts
+++
@@ -1,8 +0,0 @@
-export const tryCatch =
-  (controller: any) => async (req: any, res: any, next: any) => {
-    try {
-      await controller(req, res);
-    } catch (error) {
-      return next(error);
-    }
-  };
