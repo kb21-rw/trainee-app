a8b82fb86a8753bce7a07dcf94533f8b1607b556
divine <divinemaina@gmail.com>
2024-03-26 17:28:10 +0200

    add applicant signup with emai and password

--- backend/src/validations/applicantValidation.ts
+++ backend/src/validations/applicantValidation.ts
@@ -1,6 +1,7 @@
 import Joi from "joi";
 
 export const applicantSchema = Joi.object({
-  email: Joi.string().email(),
+  email: Joi.string().email().required(),
+  password:Joi.string().required()
 
 });
