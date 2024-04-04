e1601d189141be77921873832a1d4f89308cb4a0
Junior Hirwa <iamhirwejr@gmail.com>
2024-04-04 12:18:47 +0200

    refactor: update not found component

--- frontend/src/components/ui/NotFound.tsx
+++ frontend/src/components/ui/NotFound.tsx
@@ -9,12 +9,9 @@ const NotFound: React.FC<NotFoundProps> = ({ type }) => {
 
   return (
     <div className="w-full flex flex-col items-center justify-center bg-gray-50">
-      <div className="rounded-lg bg-white p-12 text-center shadow-xl space-y-4 w-1/4">
-        <h1 className="mb-4 text-4xl font-bold">Ooops!</h1>
+      <div className="rounded-lg bg-white p-12 text-center shadow-xl space-y-10 w-1/4">
+        <h1 className="bg-primary-dark text-white py-3.5 px-6 text-xl flex items-center justify-center font-bold">Ooops!</h1>
         <p className="text-gray-600">{message}</p>
-        <div className="w-full flex items-center justify-center">
-          <div className="bg-primary-dark text-white py-3.5 px-6 rounded-md font-medium text-base flex items-center justify-center">Try again</div>
-        </div>
       </div>
     </div>
   );
