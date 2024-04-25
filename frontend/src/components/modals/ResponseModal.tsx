d5116dfe2279f348825c0207b4414e1025a1596d
Junior Hirwa <iamhirwejr@gmail.com>
2024-04-25 10:25:15 +0200

    feat: update response modal

--- frontend/src/components/modals/ResponseModal.tsx
+++ frontend/src/components/modals/ResponseModal.tsx
@@ -1,48 +1,68 @@
-import React, { useState } from "react";
+import React from "react";
 import ModalLayout from "./ModalLayout";
 import Button from "../ui/Button";
 import { useForm } from "react-hook-form";
-import InputField from "../ui/InputField";
 import Alert from "../ui/Alert";
+import TextArea from "../ui/TextArea";
+import Cookies from "universal-cookie";
+import { useAddResponseMutation } from "../../features/user/apiSlice";
+import Loader from "../ui/Loader";
 
 const ResponseModal = ({
   closePopup,
   title,
   question,
+  questionId,
+  userId,
+  response,
+  includeButton,
 }: {
   closePopup: () => void;
   title: string;
   question: string;
+  questionId: string;
+  userId: string;
+  response?: string;
+  includeButton?: boolean;
 }) => {
-  const [response, setResponse] = useState("");
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
 
-  const handleSave = () => {
-    setResponse(response);
-    console.log("save button clicked");
-    closePopup();
+  const cookies = new Cookies();
+  const jwt = cookies.get("jwt");
+  const [addResponse, {isLoading, error, isSuccess}] = useAddResponseMutation();
+
+
+  const onSubmit = async (data: any) => {
+    await addResponse({ jwt, body: { ...data }, questionId, userId });
   };
 
-  const errorMessage: any = errors.name?.message || errors.email?.message;
+  const errorMessage: any = errors.name?.message || errors.email?.message || error?.data?.errorMessage;
 
   return (
     <ModalLayout closePopup={closePopup} title={title}>
+      {isLoading && (
+        <div className="flex items-center justify-center">
+          <Loader />
+        </div>
+      )}
       {errorMessage && <Alert type="error">{errorMessage}</Alert>}
+      {isSuccess && (
+        <Alert type="success">Response was added succesfully</Alert>
+      )}
       <form
-        onSubmit={handleSubmit(handleSave)}
+        onSubmit={handleSubmit(onSubmit)}
         className="flex flex-col gap-12 w-full"
       >
-        <InputField
-          type="text"
+        <TextArea
           label={question}
-          placeholder="Response here"
-          name="response"
-          autoFocus={true}
+          placeholder={response || "No response"}
+          name="text"
           register={register}
+          defaultValue={response}
           options={{
             required: { value: true, message: "response is required" },
             maxLength: {
@@ -52,9 +72,7 @@ const ResponseModal = ({
           }}
         />
         <div className="flex justify-end">
-          <Button variant="small" clickHandler={handleSave}>
-            Save Response
-          </Button>
+          {includeButton && <Button>Save Response</Button>}
         </div>
       </form>
     </ModalLayout>
