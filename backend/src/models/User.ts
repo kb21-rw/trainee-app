1665271ab2f6c258d577039144c8ece1ce75b7e6
Algor-Fernand <algorfernand014@gmail.com>
2024-04-25 10:56:44 +0200

    fix: case insensitive for users

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -4,9 +4,12 @@ const UserSchema = new Schema(
   {
     name: {
       type: String,
-      unique: true,
       required: true,
-    },
+      index:{
+        unique: true,
+        collation:{locale:'en',strength:2}
+    }
+  },
     email: {
       type: String,
       unique: true,
