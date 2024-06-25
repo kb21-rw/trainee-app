9e192a40e7cf60c31938bf4be5d6663608387259
Kevinemug <98740834+Kevinemug@users.noreply.github.com>
2024-06-25 12:57:03 +0200

    feat:modify question schema to handle multiple choice

--- backend/src/models/Question.ts
+++ backend/src/models/Question.ts
@@ -3,8 +3,9 @@ import { Schema, model } from "mongoose";
 export interface QuestionProperties {
   _id: string;
   title: string;
-  type: "text" | "dropdown";
+  type: "text" | "dropdown" | "multiple-choice";
   options: string[];
+  multipleChoiceOptions?: { text: string; isCorrect: boolean }[];
   responseIds: string[];
 }
 
@@ -17,11 +18,22 @@ const QuestionSchema = new Schema(
 
     type: {
       type: String,
-      enum: ["text", "dropdown"],
+      enum: ["text", "dropdown", "multiple-choice"],
       required: true,
     },
 
     options: [String],
+
+    multipleChoiceOptions: [{
+      text: {
+        type: String,
+        required: true
+      },
+      isCorrect: {
+        type: Boolean,
+        default: false
+      }
+    }],
 
     responseIds: {
       type: [Schema.Types.ObjectId],
