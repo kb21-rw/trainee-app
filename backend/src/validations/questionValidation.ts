a2d3ac5f090b92d08baa81792e79b37544d4318a
Kevinemug <98740834+Kevinemug@users.noreply.github.com>
2024-06-25 13:07:47 +0200

    feat: validation for multiple choice questions

--- backend/src/validations/questionValidation.ts
+++ backend/src/validations/questionValidation.ts
@@ -2,7 +2,7 @@ import Joi from "joi";
 
 export const createQuestionValidation = Joi.object({
   title: Joi.string().min(3).max(100).required(),
-  type: Joi.string().valid("text", "dropdown").required(),
+  type: Joi.string().valid("text", "dropdown", "multiple-choice").required(),
   options: Joi.array()
     .items(Joi.string())
     .when("type", {
@@ -10,16 +10,55 @@ export const createQuestionValidation = Joi.object({
       then: Joi.array().items(Joi.string()).min(1).required(),
       otherwise: Joi.array().items(Joi.string()).optional(),
     }),
+  multipleChoiceOptions: Joi.array()
+    .items(
+      Joi.object({
+        text: Joi.string().required(),
+        isCorrect: Joi.boolean().required(),
+      })
+    )
+    .when("type", {
+      is: "multiple-choice",
+      then: Joi.array()
+        .items(
+          Joi.object({
+            text: Joi.string().required(),
+            isCorrect: Joi.boolean().required(),
+          })
+        )
+        .min(1)
+        .required(),
+      otherwise: Joi.array().items(Joi.object()).optional(),
+    }),
 });
-
 export const updateQuestionValidation = Joi.object({
   title: Joi.string().min(3).max(100),
-  type: Joi.string().valid("text", "dropdown"),
+  type: Joi.string().valid("text", "dropdown", "multiple-choice"),
   options: Joi.array()
     .items(Joi.string())
     .when("type", {
       is: "dropdown",
       then: Joi.array().items(Joi.string()).min(1).required(),
       otherwise: Joi.array().items(Joi.string()).optional(),
+    }),
+  multipleChoiceOptions: Joi.array()
+    .items(
+      Joi.object({
+        text: Joi.string().required(),
+        isCorrect: Joi.boolean().required(),
+      })
+    )
+    .when("type", {
+      is: "multiple-choice",
+      then: Joi.array()
+        .items(
+          Joi.object({
+            text: Joi.string().required(),
+            isCorrect: Joi.boolean().required(),
+          })
+        )
+        .min(1)
+        .required(),
+      otherwise: Joi.array().items(Joi.object()).optional(),
     }),
 });
