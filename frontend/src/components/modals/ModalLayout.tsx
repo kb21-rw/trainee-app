d024be81e55ab827f793f793a2ac5d18d2ba1612
Algor-Fernand <algorfernand014@gmail.com>
2024-06-25 17:58:11 +0200

    fix: remove capitalization on title of a modal layout

--- frontend/src/components/modals/ModalLayout.tsx
+++ frontend/src/components/modals/ModalLayout.tsx
@@ -22,7 +22,7 @@ const ModalLayout = ({
     <div className="fixed inset-0 h-screen w-full flex items-center justify-center z-50 bg-secondary-dark bg-opacity-40">
       <div className="bg-white rounded-3xl flex flex-col overflow-y-auto w-[660px] max-h-[70vh] px-8 pt-9 gap-16 pb-8">
         <div className="flex">
-          <h1 className="capitalize flex-1 px-3 text-center font-bold text-3xl leading-[50px] ">
+          <h1 className="flex-1 px-3 text-center font-bold text-3xl leading-[50px] ">
             {title}
           </h1>
           <button
