252ea15e1b6ffb8e9a4e2ce96da0ae8925887af9
Junior Hirwa <iamhirwejr@gmail.com>
2024-06-27 23:13:44 +0200

    refactor: update button component

--- frontend/src/components/ui/Button.tsx
+++ frontend/src/components/ui/Button.tsx
@@ -25,6 +25,8 @@ const Button = ({
         ? "bg-red-500 text-white rounded-lg px-6"
         : variant === ButtonVariant.Small
         ? "bg-primary-dark text-white rounded-lg pl-4 pr-6"
+        : variant === ButtonVariant.Edit
+        ? "bg-primary-dark text-white rounded-lg px-6 flex items-center justify-center"
         : "bg-primary-dark text-white rounded-lg w-full"
     } py-4 font-medium text-base flex items-center justify-center text-center cursor-pointer`}
       type={type}
