a176388e510d2a675bda46fd3dcc17077d35aa09
Junior Hirwa <iamhirwejr@gmail.com>
2024-07-25 17:26:53 +0200

    feat: add cohort elements

--- frontend/src/utils/data.ts
+++ frontend/src/utils/data.ts
@@ -3,14 +3,16 @@ export const adminMenu = [
   { link: "/forms", title: "Forms" },
   { link: "/trainees", title: "Trainees" },
   { link: "/coaches", title: "Coaches" },
+  { link: "/cohorts", title: "Cohorts" },
 ];
 export const coachMenu = [
   { link: "/", title: "Overview" },
-  { link: "/trainees-results", title: "Trainee Results"},
+  { link: "/trainees-results", title: "Trainee Results" },
   { link: "/my-trainees", title: "My trainees" },
 ];
 
 export const usersPerPageValues = [10, 20, 30, 40, 50, 100];
+export const cohortsPerPage = [10, 20, 30, 40, 50, 100];
 export const coachTableSortingValues = [
   { title: "Entry", value: "entry" },
   { title: "Name", value: "name" },
@@ -22,6 +24,16 @@ export const traineeTableSortingValues = [
   { title: "Entry", value: "entry" },
   { title: "Name", value: "name" },
 ];
+
+export const cohortTableSortingValues = [
+  { title: "Entry", value: "entry" },
+  { title: "Name", value: "name" },
+  { title: "Date", value: "date" },
+];
+
+export const cohortTableHeaders = ["No", "Cohort Name", "Trainees", "Action"]
+export const cohortTableDataItems = ["_id", "name", "trainees", "action"];
+
 export const traineeTableHeaders = ["No", "Name", "Email", "Coach", "Action"];
 export const editTraineeTableHeaders = ["No", "Name", "Email", ""];
 export const editTraineeTableItems = ["_id", "name", "email"];
