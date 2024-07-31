9df44f1d6a6fccdfd39bb21dd3bddc439f4287b0
Junior Hirwa <iamhirwejr@gmail.com>
2024-07-31 10:16:40 +0200

    fix: add getApplicants which was accidently removed

--- backend/src/controllers/applicantControllers.ts
+++ backend/src/controllers/applicantControllers.ts
@@ -1,19 +1,45 @@
-import { NextFunction, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { applicationDecisionSchema } from "../validations/applicantValidation";
-import { applicantDecisionService } from "../services/applicantService";
+import {
+  applicantDecisionService,
+  getApplicantsService,
+} from "../services/applicantService";
 
 export const decision = async (
-    req: any,
-    res: Response,
-    next: NextFunction
-  ) => {
-    try {
-      const user = req.user;
-      const body = req.body;
-      await applicationDecisionSchema.validateAsync(body);
-      const decision = await applicantDecisionService(user, body);
-      return res.status(201).send(decision);
-    } catch (error: any) {
-      next(error);
-    }
-  };
+  req: any,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const user = req.user;
+    const body = req.body;
+    await applicationDecisionSchema.validateAsync(body);
+    const decision = await applicantDecisionService(user, body);
+    return res.status(201).send(decision);
+  } catch (error: any) {
+    next(error);
+  }
+};
+
+export const getApplicants = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const nameSearch = req.query.nameSearch ? String(req.query.nameSearch) : "";
+    const applicantsPerPage = req.query.applicantsPerPage
+      ? Number(req.query.applicantsPerPage)
+      : 10;
+    const sortBy = req.query.sortBy ? String(req.query.sortBy) : "userId";
+
+    const applicants = await getApplicantsService({
+      nameSearch,
+      applicantsPerPage,
+      sortBy,
+    });
+    return res.status(201).send(applicants);
+  } catch (error: any) {
+    next(error);
+  }
+};
