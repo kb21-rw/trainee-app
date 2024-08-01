a2c6c313edc85f9810f022b931341512686030fc
Algor-Fernand <algorfernand014@gmail.com>
2024-08-01 11:58:24 +0200

    fix: navigate to login

--- frontend/src/pages/Applicant/ThankYouNote.tsx
+++ frontend/src/pages/Applicant/ThankYouNote.tsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 const ThankYouNote: React.FC = () => {
   const navigate = useNavigate();
 
-  const handleGoHome = () => navigate("/applicant");
+  const handleLogin = () => navigate("/login");
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-blue-50">
@@ -12,21 +12,20 @@ const ThankYouNote: React.FC = () => {
         <h1 className="text-5xl font-bold text-blue-400 mb-8 font-thank-you">
           Thank you!
         </h1>
-        <p className="text-primary-dark mb-10">
+        <p className="text-primary-dark mb-3">
           Thanks for signing up to The Gym -{" "}
-          <span className="font-bold">
+          <span className="font-bold ">
             Rwanda&#39;s most intensive platform.
           </span>
-          <br />
-          <span>
-            Check your email inbox, a confirmation email has been sent to you.
-          </span>
         </p>
+          <p className="text-xl text-primary-dark mb-5">
+            Check your email inbox, a confirmation email has been sent.
+          </p>
         <button
-          onClick={handleGoHome}
+          onClick={handleLogin}
           className="px-6 py-2 bg-blue-500 text-white rounded-full hover:bg-blue-600 transition duration-300"
         >
-          Go Home
+          Login
         </button>
       </div>
     </div>
