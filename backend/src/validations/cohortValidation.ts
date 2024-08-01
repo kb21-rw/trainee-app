6e6defc3072a56a1e2c0ef7fb5f876791f2a9ecd
Kevinemug <98740834+Kevinemug@users.noreply.github.com>
2024-08-01 11:43:02 +0200

    feat: add validation

--- backend/src/validations/cohortValidation.ts
+++ backend/src/validations/cohortValidation.ts
@@ -3,5 +3,14 @@ import Joi from "joi";
 export const createCohortValidation = Joi.object({
   name: Joi.string().min(3).max(100).required(),
   description: Joi.string().min(3).max(100).optional(),
-
+  startDate: Joi.date().optional().messages({
+    "date.base": "Start date must be a valid date",
+  }),
+  endDate: Joi.date()
+    .greater(Joi.ref('startDate'))
+    .optional()
+    .messages({
+      "date.base": "End date must be a valid date",
+      "date.greater": "End date must be later than start date",
+    }),
 });
