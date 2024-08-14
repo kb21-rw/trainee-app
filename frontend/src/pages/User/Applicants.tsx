12e05e94daf079434580c1c28a67cbc116d843c5
Junior Hirwa <iamhirwejr@gmail.com>
2024-08-14 10:04:30 +0200

    feat: add applicants page

---
+++ frontend/src/pages/User/Applicants.tsx
@@ -0,0 +1,52 @@
+/* eslint-disable no-unused-vars */
+import React, { useState } from "react";
+import { useGetApplicantsQuery } from "../../features/user/apiSlice";
+import { getApplicants,getJWT } from "../../utils/helper";
+import UserTableHeader from "../../components/ui/UserTableHeader";
+import {
+    applicantTableDataItems,
+    applicantTableHeaders,
+  applicantTableSortingValues,
+  applicantsPerPage,
+} from "../../utils/data";
+import UserTable from "../../components/ui/UserTable";
+
+const Applicants = () => {
+  const jwt: string = getJWT();
+  const [query, setQuery] = useState("");
+  const { data, isFetching: isGettingAllApplicantsLoading, isError } = useGetApplicantsQuery({
+    jwt,
+    query
+  });
+
+
+    const applicantsData = data?.map((item:any) => (item.applicants))
+
+    const Applicantlist = getApplicants(applicantsData?.[0], applicantTableDataItems)
+    console.log(Applicantlist);
+
+  
+  
+  return (
+    <div className="py-12 space-y-5">
+      <UserTableHeader
+        setQuery={setQuery}
+        sortingValues={applicantTableSortingValues}
+        usersPerPageValues={applicantsPerPage}
+        userType="Applicant"
+      />
+      <UserTable
+        headers={applicantTableHeaders}
+        data={Applicantlist}
+        actions={[
+            { type: "accept", actionCaller: () => {} },
+            { type: "reject", actionCaller: () => {} },
+        ]}
+        isLoading={isGettingAllApplicantsLoading}
+        response={true}
+      />
+    </div>
+  );
+};
+
+export default Applicants;
