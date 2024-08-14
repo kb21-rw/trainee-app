804e7bb53c22bdca41d1c3992e68377d2efff78d
Junior Hirwa <iamhirwejr@gmail.com>
2024-08-14 10:06:46 +0200

    feat: add action buttons

--- frontend/src/components/ui/UserTable.tsx
+++ frontend/src/components/ui/UserTable.tsx
@@ -4,10 +4,12 @@ import Edit from "../../assets/EditIcon";
 import Delete from "../../assets/DeleteIcon";
 import Loader from "../../components/ui/Loader";
 import NotFound from "./NotFound";
+import { FaFileAlt } from "react-icons/fa";
 interface PropTypes {
   headers: string[];
   isLoading: boolean;
   data: string[][];
+  response?: boolean;
   actions?: {
     type: string;
     actionCaller:
@@ -16,7 +18,13 @@ interface PropTypes {
   }[];
 }
 
-const UserTable = ({ headers, isLoading, data, actions }: PropTypes) => {
+const UserTable = ({
+  headers,
+  isLoading,
+  data,
+  response,
+  actions,
+}: PropTypes) => {
   return (
     <table className="w-full my-8 table-auto object-fit">
       <thead className="bg-primary-light bg-opacity-20 h-20">
@@ -24,7 +32,7 @@ const UserTable = ({ headers, isLoading, data, actions }: PropTypes) => {
           {headers.map((header: string, index: number) => (
             <td
               key={index}
-              className="first:rounded-l-xl last:rounded-r-xl pl-10"
+              className="first:rounded-l-xl last:rounded-r-xl pl-12 text-center"
             >
               {header}
             </td>
@@ -39,7 +47,7 @@ const UserTable = ({ headers, isLoading, data, actions }: PropTypes) => {
             </td>
           </tr>
         </tbody>
-      ) : data.length === 0 ? (
+      ) : data?.length === 0 ? (
         <tbody>
           <tr className="flex w-screen h-[50vh]">
             <td className="w-full h-auto">
@@ -49,27 +57,49 @@ const UserTable = ({ headers, isLoading, data, actions }: PropTypes) => {
         </tbody>
       ) : (
         <tbody>
-          {data.map((userData: string[], index: number) => (
+          {data?.map((userData: string[], index: number) => (
             <tr key={userData[0]} className="border-b border-black h-[100px]">
-              <td className="text-base font-medium pl-10">{index + 1}</td>
+              <td className="text-base font-medium pl-10 text-center">
+                {index + 1}
+              </td>
               {userData.slice(1).map((item: string, index: number) => (
-                <td key={index} className="text-base font-medium pl-12">
+                <td
+                  key={index}
+                  className="text-base font-medium pl-12 text-center"
+                >
                   {item}
                 </td>
               ))}
+              {response && (
+                <td className="text-base font-medium pl-28 text-center">
+                  <span className="cursor-pointer">
+                    <FaFileAlt className="text-2xl hover:text-primary-dark"/>
+                  </span>
+                </td>
+              )}
               <td className="text-base font-medium">
-                <div className="flex items-center gap-4 h-full pl-8">
+                <div className="flex items-center justify-center gap-4 h-full">
                   {actions?.map((action: any, index: number) => (
                     <button
                       key={index}
                       onClick={() =>
                         action.actionCaller(
-                          action.type == "delete" ? userData[0] : userData
+                          action.type == "delete" ? userData[0] : userData,
                         )
                       }
                     >
                       {action.type == "delete" && <Delete />}
                       {action.type == "edit" && <Edit />}
+                      {action.type == "accept" && (
+                        <span className="px-8 py-2 bg-green-400/80 text-white rounded">
+                          Accept
+                        </span>
+                      )}
+                      {action.type == "reject" && (
+                        <span className="px-8 py-2 bg-red-400 text-white rounded">
+                          Reject
+                        </span>
+                      )}
                     </button>
                   ))}
                 </div>
