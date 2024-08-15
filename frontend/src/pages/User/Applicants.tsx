135587dc5c369f3657ed08e4becf7b1cff815675
Junior Hirwa <iamhirwejr@gmail.com>
2024-08-15 18:09:34 +0200

    refactor: use renamed component

--- frontend/src/pages/User/Applicants.tsx
+++ frontend/src/pages/User/Applicants.tsx
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
-import {
-  useGetApplicantsQuery,
-} from "../../features/user/apiSlice";
+import { useGetApplicantsQuery } from "../../features/user/apiSlice";
 import { getApplicants, getJWT } from "../../utils/helper";
 import UserTableHeader from "../../components/ui/UserTableHeader";
 import {
@@ -11,16 +9,20 @@ import {
   applicantsPerPage,
 } from "../../utils/data";
 import UserTable from "../../components/ui/UserTable";
-import RejectOrDropModal from "../../components/modals/RejectOrDropModal";
-import {AiOutlineWarning} from 'react-icons/ai';
+import DropOrEnrollModal from "../../components/modals/DropOrEnrollModal";
+import { AiOutlineWarning } from "react-icons/ai";
+import { ApplicantDecision } from "../../utils/types";
 
 const Applicants = () => {
   const jwt: string = getJWT();
   const [query, setQuery] = useState("");
-  const [userToBeRejectedId, setUserToBeRejectedId] = useState<string | null>(
+  const [decideForUserId, setDecideForUserId] = useState<string | null>(
     null,
   );
-  const [isRejectUserModalOpen, setIsRejectUserModalOpen] = useState<boolean>(false);
+  const [isRejectUserModalOpen, setIsRejectUserModalOpen] =
+    useState<boolean>(false);
+  const [isAcceptUserModalOpen, setIsAcceptUserModalOpen] =
+    useState<boolean>(false);
   const {
     data,
     isFetching: isGettingAllApplicantsLoading,
@@ -37,31 +39,33 @@ const Applicants = () => {
     applicantTableDataItems,
   );
 
-  const userToBeRejected = applicantList?.find(
-    (user) => user[0] == userToBeRejectedId,
+  const userDecision = applicantList?.find(
+    (user) => user[0] == decideForUserId,
   );
 
-  const userToBeRejectedIdName = userToBeRejected ? userToBeRejected[2] : "";
-  const userToBeRejectedEmail = userToBeRejected ? userToBeRejected[3] : "";
 
-  if(isError) {
+  const userName = userDecision ? userDecision[2] : "";
+  const userEmail = userDecision ? userDecision[3] : "";
+
+
+  if (isError) {
     return (
       <div className="flex flex-col items-center justify-center p-6 bg-red-50 border border-red-200 rounded-lg text-center">
-      <AiOutlineWarning className="text-red-500 text-4xl mb-4" />
-      <p className="text-red-500 font-semibold text-lg">
-        Error fetching applicants
-      </p>
-      <p className="text-gray-600 mb-4">
-        Something went wrong while trying to load the data.
-      </p>
-      <button
-        onClick={() => window.location.reload()}
-        className="px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600 transition"
-      >
-        Retry
-      </button>
-    </div>
-    )
+        <AiOutlineWarning className="text-red-500 text-4xl mb-4" />
+        <p className="text-red-500 font-semibold text-lg">
+          Error fetching applicants
+        </p>
+        <p className="text-gray-600 mb-4">
+          Something went wrong while trying to load the data.
+        </p>
+        <button
+          onClick={() => window.location.reload()}
+          className="px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600 transition"
+        >
+          Retry
+        </button>
+      </div>
+    );
   }
 
   return (
@@ -79,21 +83,38 @@ const Applicants = () => {
           {
             type: "reject",
             actionCaller: async (user: string) => {
-              await setUserToBeRejectedId(user[0]);
+              await setDecideForUserId(user[0]);
               setIsRejectUserModalOpen(true);
             },
           },
-          { type: "accept", actionCaller: () => {} },
+          {
+            type: "accept",
+            actionCaller: async (user: string) => {
+              await setDecideForUserId(user[0]);
+              setIsAcceptUserModalOpen(true);
+            },
+          },
         ]}
         isLoading={isGettingAllApplicantsLoading}
         hasResponse={true}
       />
       {isRejectUserModalOpen && (
-        <RejectOrDropModal
+        <DropOrEnrollModal
           closePopup={() => setIsRejectUserModalOpen(false)}
-          userName={userToBeRejectedIdName}
-          userEmail={userToBeRejectedEmail}
-          userToBeRejectedId={userToBeRejectedId}
+          userName={userName}
+          userEmail={userEmail}
+          decideForUserId={decideForUserId}
+          decision={ApplicantDecision.Rejected}
+          jwt={jwt}
+        />
+      )}
+      {isAcceptUserModalOpen && (
+        <DropOrEnrollModal
+          closePopup={() => setIsAcceptUserModalOpen(false)}
+          userName={userName}
+          userEmail={userEmail}
+          decideForUserId={decideForUserId}
+          decision={ApplicantDecision.Accepted}
           jwt={jwt}
         />
       )}
