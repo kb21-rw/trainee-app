661f2fa209576b204f7f88ddd20804f0d8f118b0
David Gusenga <gsngdavid@gmail.com>
2024-09-01 11:51:22 +0200

    chore: update form creation validations

--- backend/src/validations/formValidation.ts
+++ backend/src/validations/formValidation.ts
@@ -4,7 +4,34 @@ import { FormType } from "../utils/types";
 export const createFormValidation = Joi.object({
   name: Joi.string().min(3).max(100).required(),
   description: Joi.string().min(3).max(100),
-  type: Joi.string().valid(FormType.Application, FormType.Applicant, FormType.Trainee).required()
+  type: Joi.string()
+    .valid(FormType.Application, FormType.Applicant, FormType.Trainee)
+    .required(),
+  startDate: Joi.when("type", {
+    is: FormType.Application,
+    then: Joi.date().required(),
+    otherwise: Joi.forbidden(),
+  }),
+  endDate: Joi.when("type", {
+    is: FormType.Application,
+    then: Joi.date().required(),
+    otherwise: Joi.forbidden(),
+  }),
+  stages: Joi.when("type", {
+    is: FormType.Application,
+    then: Joi.array().items(
+      Joi.object({
+        id: Joi.string()
+          .hex()
+          .length(24)
+          .message("stageId is not valid")
+          .optional(),
+        name: Joi.string().min(1).required(),
+        description: Joi.string(),
+      })
+    ),
+    otherwise: Joi.forbidden(),
+  }),
 });
 
 export const editFormValidation = Joi.object({
