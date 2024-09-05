f5fd7d79f26c830d3680a3451918f420fc967c9a
David Gusenga <gsngdavid@gmail.com>
2024-09-05 15:44:45 +0200

    feat: add getApplicationForm

--- backend/src/utils/helpers/cohort.ts
+++ backend/src/utils/helpers/cohort.ts
@@ -1,9 +1,15 @@
 import { Types } from "mongoose";
 import CustomError from "../../middlewares/customError";
-import Cohort from "../../models/Cohort";
-import { COHORT_NOT_FOUND, DUPLICATE_DOCUMENT } from "../errorCodes";
+import Cohort, { ICohort } from "../../models/Cohort";
+import {
+  COHORT_NOT_FOUND,
+  DUPLICATE_DOCUMENT,
+  FORM_NOT_FOUND,
+  NOT_ALLOWED,
+} from "../errorCodes";
 import { IStage } from "../types";
 import { SetOptional } from "type-fest";
+import Form, { IForm } from "../../models/Form";
 
 export const getCurrentCohort = async () => {
   const currentCohort = await Cohort.findOne({ isActive: true });
@@ -13,6 +19,26 @@ export const getCurrentCohort = async () => {
   }
 
   return currentCohort;
+};
+
+export const getApplicationForm = async (currentCohort: ICohort) => {
+  if (!currentCohort.applicationForm.id) {
+    throw new CustomError(NOT_ALLOWED, "Applications aren't open yet", 401);
+  }
+
+  const applicationForm = await Form.findById<IForm>(
+    currentCohort.applicationForm.id
+  );
+
+  if (!applicationForm) {
+    throw new CustomError(
+      FORM_NOT_FOUND,
+      "Something wrong, our team is trying to fix it",
+      500
+    );
+  }
+
+  return applicationForm;
 };
 
 export const updateStagesHandler = (
