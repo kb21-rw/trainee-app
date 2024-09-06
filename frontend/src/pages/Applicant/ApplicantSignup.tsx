748aebe6ed09f5a257af0c434e7b897e3f9b78f2
Junior Hirwa <iamhirwejr@gmail.com>
2024-09-06 12:06:12 +0200

    feat: add password confirmation

--- frontend/src/pages/Applicant/ApplicantSignup.tsx
+++ frontend/src/pages/Applicant/ApplicantSignup.tsx
@@ -5,19 +5,23 @@ import Cookies from "universal-cookie";
 import { Link, useNavigate } from "react-router-dom";
 import InputField from "../../components/ui/InputField";
 import Button from "../../components/ui/Button";
-import { H1 } from "../../components/ui/Typography";
+import { H2 } from "../../components/ui/Typography";
 import Loader from "../../components/ui/Loader";
+import { ButtonSize } from "../../utils/types";
 
 const ApplicantSignup = () => {
   const [signup, { isLoading, error }] = useSignupMutation();
   const {
     register,
     handleSubmit,
+    watch,
     formState: { errors },
   } = useForm();
 
   const cookies = new Cookies();
   const navigate = useNavigate();
+
+  const password = watch("password");
 
   const onSubmit = async (userData: any) => {
     const result = await signup({
@@ -34,22 +38,25 @@ const ApplicantSignup = () => {
   const errorMessage: any =
     errors.email?.message ||
     errors.password?.message ||
-    error?.userData?.errorMessage;
+    errors["confirm-password"]?.message ||
+    error?.data?.errorMessage;
 
   return (
     <form
       onSubmit={handleSubmit(onSubmit)}
-      className="flex flex-col h-screen justify-center gap-5 md:gap-16 items-center px-5 sm:px-10 md:p-0 max-w-md mx-auto"
+      className="flex flex-col h-screen justify-center gap-5 md:gap-16  px-5 sm:px-10 md:p-0 max-w-xl mx-auto"
     >
-      <H1>Sign Up</H1>
-      {isLoading && <Loader />}
-      <div className="space-y-3 md:space-y-6 lg:space-y-10 w-full">
+      <div className="text-center">
+        <H2>Sign Up</H2>
+      </div>
+      <div className="w-full flex text-center justify-center">{isLoading && <Loader />}</div>
+      <div className="space-y-3 md:space-y-6 lg:space-y-7 w-full">
         {errorMessage && (
           <div className="py-2 bg-error-light text-error-dark flex justify-center items-center rounded-lg">
             {errorMessage}
           </div>
         )}
-        <div className="flex flex-col gap-5 mx-24">
+        <div className="flex flex-col gap-5">
           <InputField
             name="name"
             type="text"
@@ -91,18 +98,35 @@ const ApplicantSignup = () => {
                 value:
                   /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
                 message:
-                  "Password must be at least 8 characters long and include :uppercase, lowercase, number, and special character.",
+                  "Password must be 8+ characters with uppercase, lowercase, number, and special character.",
               }, // checks if password is valid
+            }}
+            errors={errors}
+          />
+          <InputField
+            name="confirm-password"
+            type="password"
+            label="Confirm Password"
+            placeholder="Re-enter password"
+            register={register}
+            options={{
+              required: {
+                value: true,
+                message: "Password confirmation is a required field",
+              },
+              validate: (value: string) =>
+                value === password || "Passwords do not match",
             }}
             errors={errors}
           />
         </div>
       </div>
-      <Button type="submit">Sign Up</Button>
-
+      <Button size={ButtonSize.Large} type="submit">
+        Sign Up
+      </Button>
       <div className="flex gap-2">
         <span>Already have an account?</span>
-        <Link to="/applicant/signin" className="text-primary-dark">
+        <Link to="/login" className="text-primary-dark">
           Login
         </Link>
       </div>
