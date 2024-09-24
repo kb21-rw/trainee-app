eee4b8ca26c57688fc74c45f1e815707c83813d0
Junior Hirwa <iamhirwejr@gmail.com>
2024-09-24 16:21:55 +0200

    refactor: udpate all  forms page

--- frontend/src/pages/Form/AllForms.tsx
+++ frontend/src/pages/Form/AllForms.tsx
@@ -1,71 +1,30 @@
 import React, { useState } from "react";
 import SearchInput from "../../components/ui/SearchInput";
 import {
-  useCreateFormMutation,
   useGetAllFormsQuery,
 } from "../../features/user/apiSlice";
 import FormCard from "../../components/ui/FormCard";
 import { IFormType } from "../../utils/types";
 import Loader from "../../components/ui/Loader";
-import { useNavigate } from "react-router-dom";
 import NotFound from "../../components/ui/NotFound";
-import Button from "../../components/ui/Button";
-import PlusIcon from "../../assets/PlusIcon";
-import { getJWT, getNextFormTitle } from "../../utils/helper";
+import { getJWT } from "../../utils/helper";
+import CreateFormDropdown from "../../components/ui/createFormDropdown";
 
 const AllForms = () => {
   const [searchQuery, setSearchQuery] = useState("");
-  const navigate = useNavigate();
   const jwt: string = getJWT();
-  const [createForm] = useCreateFormMutation();
-  const [clicked, setClick] = useState(false);
   const { data, isFetching } = useGetAllFormsQuery({
     jwt,
     searchString: searchQuery,
   });
+
   const forms=data?.forms;
-
-  const onClickAddForm = async (type?: "Applicant") => {
-    const nextFormTitle = getNextFormTitle(forms);
-    let requestBody: object = { title: nextFormTitle};
-    if (type) {
-      requestBody = { ...requestBody, type };
-    }
-
-    const {data:formData} = await createForm({
-      jwt,
-      body: requestBody,
-    });
-    const id = formData?._id;
-    if (clicked) {
-      navigate(`/forms/${id}`);
-    } 
-  };
 
   return (
     <div className="py-12">
       <div className="flex justify-between items-center my-5">
         <SearchInput setSearchQuery={setSearchQuery} />  
-        <div className="grid gap-1">
-          <Button onClick={() => setClick(!clicked)}>
-            <div className="flex items-center right-0">
-              <PlusIcon /> <span>Add form</span>
-            </div>
-          </Button>
-          <div
-            className={
-              !clicked
-                ? "hidden"
-                : `grid text-white bg bg-primary-dark rounded-lg`
-            }
-          >
-            <Button onClick={() => onClickAddForm("Applicant")}>
-              Applicants
-            </Button>
-            <div className="border w-6/6"></div>
-            <Button onClick={() => onClickAddForm()}>Trainee</Button>
-          </div>
-        </div>
+        <CreateFormDropdown />
       </div>
       {isFetching ? (
         <div className="h-[50vh] flex items-center justify-center">
