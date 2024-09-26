bc675be5a92b3edd5813ea794def4e8b21be6ea4
Junior Hirwa <iamhirwejr@gmail.com>
2024-09-26 10:08:09 +0200

    refactor: refactor the shadow utility class

--- frontend/src/components/ui/CreateFormDropdown.tsx
+++ frontend/src/components/ui/CreateFormDropdown.tsx
@@ -55,7 +55,7 @@ export default function CreateFormDropdown() {
         <MenuItems
           transition
           anchor="bottom end"
-          className="origin-top-right rounded-lg border border-white/5 bg-white text-sm text-gray-500 font-medium transition duration-100 ease-out [--anchor-gap:var(--spacing-1)] focus:outline-none data-[closed]:scale-95 data-[closed]:opacity-0 shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)]"
+          className="origin-top-right rounded-lg border border-white/5 bg-white text-sm text-gray-500 font-medium transition duration-100 ease-out [--anchor-gap:var(--spacing-1)] focus:outline-none data-[closed]:scale-95 data-[closed]:opacity-0 custom-shadow"
         >
           {menuItems.map((item, index) => (
             <MenuItem key={index}>
