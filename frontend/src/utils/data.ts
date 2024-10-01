4c6be21753abad29c558a3e5d74465911a0e0940
Junior Hirwa <iamhirwejr@gmail.com>
2024-10-01 14:57:32 +0200

    refactor: update type

--- frontend/src/utils/data.ts
+++ frontend/src/utils/data.ts
@@ -1,4 +1,4 @@
-import { MenuItemProps } from "./types";
+import { MenuItemType } from "./types";
 
 export const adminMenu = [
   { link: "/", title: "Overview" },
@@ -18,12 +18,15 @@ export const applicantMenu = [
   { link: "/home", title: "Home" },
   { link: "/apply", title: "Apply" },
   { link: "/saved-application", title: "Saved Application" },
-]
+];
 
-export const menuItems: MenuItemProps[] = [
+export const menuItems: MenuItemType[] = [
   { label: "Create a new Trainee form", type: "Trainee" },
   { label: "Create a new Applicant form", type: "Applicant" },
-  { label: "Create a new Application form", link: "/forms/create/application-form" },
+  {
+    label: "Create a new Application form",
+    link: "/forms/create/application-form",
+  },
 ];
 
 export const usersPerPageValues = [10, 20, 30, 40, 50, 100];
@@ -50,12 +53,34 @@ export const applicantTableSortingValues = [
   { title: "Entry", value: "entry" },
   { title: "Name", value: "name" },
   { title: "Email", value: "email" },
-]
+];
 
-export const cohortTableHeaders = ["No", "Cohort Name", "Stages", "Applicants", "Trainees", "Dropped", "Action"]
-export const cohortTableDataItems = ["_id", "name", "stages", "applicants", "trainees", "rejected"];
-export const applicantTableHeaders = ["No", "Applicant Number", "Applicant Name", "Email", "Response", "Action"];
-export const applicantTableDataItems = ["_id","userId", "name", "email"];
+export const cohortTableHeaders = [
+  "No",
+  "Cohort Name",
+  "Stages",
+  "Applicants",
+  "Trainees",
+  "Dropped",
+  "Action",
+];
+export const cohortTableDataItems = [
+  "_id",
+  "name",
+  "stages",
+  "applicants",
+  "trainees",
+  "rejected",
+];
+export const applicantTableHeaders = [
+  "No",
+  "Applicant Number",
+  "Applicant Name",
+  "Email",
+  "Response",
+  "Action",
+];
+export const applicantTableDataItems = ["_id", "userId", "name", "email"];
 export const traineeTableHeaders = ["No", "Name", "Email", "Coach", "Action"];
 export const editTraineeTableHeaders = ["No", "Name", "Email", ""];
 export const editTraineeTableItems = ["_id", "name", "email"];
