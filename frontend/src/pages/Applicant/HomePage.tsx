cac238eb2e25836aa25d89c1bba35e218ddcc3cb
Junior Hirwa <iamhirwejr@gmail.com>
2024-10-09 15:12:03 +0200

    feat: add loader and no open application component

--- frontend/src/pages/Applicant/HomePage.tsx
+++ frontend/src/pages/Applicant/HomePage.tsx
@@ -1,25 +1,31 @@
-/* eslint-disable no-unused-vars */
 import React from "react";
 import { Link } from "react-router-dom";
-import { ButtonSize, ButtonVariant } from "../../utils/types";
+import { ApplicationFormStatus, ButtonSize, ButtonVariant } from "../../utils/types";
 import Button from "../../components/ui/Button";
 import CohortInfo from "../../components/ui/CohortInfo";
 import ApplicationStatus from "../../components/ui/ApplicationStatus";
-import { deadLineExceededInfo } from "../../utils/data";
+import { deadLineExceededInfo, noOpenApplicationInfo } from "../../utils/data";
 import { useGetFormForApplicantsQuery } from "../../features/user/apiSlice";
 import { applicationStatusHandler, getJWT } from "../../utils/helper";
+import Loader from "../../components/ui/Loader";
 
 const HomePage = () => {
   const jwt: string = getJWT();
-  const {data: applicationForm} = useGetFormForApplicantsQuery(jwt)
+  const { data: applicationForm, isLoading } =
+    useGetFormForApplicantsQuery(jwt);
 
-  const {isApplicationOpen, isDeadlineExceeded} = applicationStatusHandler(applicationForm?.startDate, applicationForm?.endDate)
-
-   
+  const applicationStatus = applicationStatusHandler(applicationForm);
+  console.log(applicationStatus);
+  console.log('application form', applicationForm)
 
   return (
-    <div className="h-full flex flex-col items-center justify-between mt-4 w-full">
-      {isApplicationOpen && (
+    <div className="h-full flex flex-col items-center justify-center w-full">
+      {isLoading && (
+        <div className="flex items-center justify-center w-full h-screen">
+          <Loader />
+        </div>
+      )}
+      {applicationStatus.status === ApplicationFormStatus.OPEN && (
         <div className="md:container md:mx-auto px-6 flex flex-col items-center justify-center">
           <CohortInfo />
           <div className="my-10 flex items-center justify-center">
@@ -31,12 +37,20 @@ const HomePage = () => {
           </div>
         </div>
       )}
-      {isDeadlineExceeded && (
+      {applicationStatus.status === ApplicationFormStatus.DEADLINE_PASSED && (
         <ApplicationStatus
           heading={deadLineExceededInfo.heading}
           description={deadLineExceededInfo.description}
           buttonLink={deadLineExceededInfo.buttonLink}
           buttonText={deadLineExceededInfo.buttonText}
+        />
+      )}
+      {applicationStatus.status === ApplicationFormStatus.NO_APPLICATION && (
+        <ApplicationStatus
+          heading={noOpenApplicationInfo.heading}
+          description={noOpenApplicationInfo.description}
+          buttonLink={noOpenApplicationInfo.buttonLink}
+          buttonText={noOpenApplicationInfo.buttonText}
         />
       )}
     </div>
