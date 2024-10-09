d01beb9d1c91bc8722fff2370a9a0fa39cf7f3fe
Junior Hirwa <iamhirwejr@gmail.com>
2024-10-09 14:05:57 +0200

    refactor: update homepage - remove mock-data

--- frontend/src/pages/Applicant/HomePage.tsx
+++ frontend/src/pages/Applicant/HomePage.tsx
@@ -1,35 +1,44 @@
 /* eslint-disable no-unused-vars */
-
 import React from "react";
 import { Link } from "react-router-dom";
 import { ButtonSize, ButtonVariant } from "../../utils/types";
 import Button from "../../components/ui/Button";
 import CohortInfo from "../../components/ui/CohortInfo";
+import ApplicationStatus from "../../components/ui/ApplicationStatus";
+import { deadLineExceededInfo } from "../../utils/data";
+import { useGetFormForApplicantsQuery } from "../../features/user/apiSlice";
+import { applicationStatusHandler, getJWT } from "../../utils/helper";
 
 const HomePage = () => {
-  
-  const application = {
-    description: "This is a new application form for the gym cohort 5",
-    endDate: "2024-10-10T22:00:00.000Z",
-    name: "Application form for Cohort 5 2025",
-    questions: [],
-    stages: [{}, {}],
-    startDate: "2024-10-04T22:00:00.000Z",
-    type: "Application",
-  };
+  const jwt: string = getJWT();
+  const {data: applicationForm} = useGetFormForApplicantsQuery(jwt)
+
+  const {isApplicationOpen, isDeadlineExceeded} = applicationStatusHandler(applicationForm?.startDate, applicationForm?.endDate)
+
+   
 
   return (
-    <div className="h-full flex flex-col items-center justify-between mt-12">
-      <div className="container mx-auto px-6 flex flex-col itemes-center justify-center">
-        <CohortInfo />
-        <div className="my-20 flex items-center justify-center">
-          <div className="container mx-auto w-1/5">
-            <Button variant={ButtonVariant.Primary} size={ButtonSize.Large}>
-              <Link to="/apply">Apply now</Link>
-            </Button>
+    <div className="h-full flex flex-col items-center justify-between mt-4 w-full">
+      {isApplicationOpen && (
+        <div className="md:container md:mx-auto px-6 flex flex-col items-center justify-center">
+          <CohortInfo />
+          <div className="my-10 flex items-center justify-center">
+            <div>
+              <Button variant={ButtonVariant.Primary} size={ButtonSize.Large}>
+                <Link to="/apply">Apply now</Link>
+              </Button>
+            </div>
           </div>
         </div>
-      </div>
+      )}
+      {isDeadlineExceeded && (
+        <ApplicationStatus
+          heading={deadLineExceededInfo.heading}
+          description={deadLineExceededInfo.description}
+          buttonLink={deadLineExceededInfo.buttonLink}
+          buttonText={deadLineExceededInfo.buttonText}
+        />
+      )}
     </div>
   );
 };
