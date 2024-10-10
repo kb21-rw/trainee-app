8e856bb2dc1b8a486c332490b1c4039ef07adfbb
Junior Hirwa <iamhirwejr@gmail.com>
2024-10-10 11:46:06 +0200

    refactor: change Button to anchor tag

--- frontend/src/components/ui/ApplicationStatus.tsx
+++ frontend/src/components/ui/ApplicationStatus.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import Button from "./Button";
 
 interface ApplicationStatusProps {
   heading?: string;
@@ -16,35 +14,31 @@ const ApplicationStatus: React.FC<ApplicationStatusProps> = ({
   buttonLink,
 }) => {
   return (
-    <div>
+    <>
       <div className="text-center flex items-center flex-col">
-        <div>
-          <h1 className="text-4xl font-bold text-gray-800 mb-8 text-center">
-            {heading}
-          </h1>
-        </div>
+        <h1 className="text-2xl font-medium text-gray-600 mb-8 text-center">
+          {heading}
+        </h1>
       </div>
       <div className="max-w-2xl mx-auto flex flex-col items-center justify-center border border-gray-300 rounded-lg p-6 shadow-lg space-y-4">
         <p className="text-center text-gray-500">{description}</p>
 
-        <div>
-          <Button>
-            <Link to={buttonLink}>{buttonText}</Link>
-          </Button>
-        </div>
+        <a
+          className="bg-primary-dark text-white px-6 py-3 rounded-md"
+          href={buttonLink}
+        >
+          {buttonText}
+        </a>
 
         <div className="text-gray-600 text-sm">
           Learn more about The Gym software developer trainee program{" "}
-          <a
-            href="https://www.the-gym.rw/"
-            className="text-blue-600 underline"
-          >
+          <a href="https://www.the-gym.rw/" className="text-blue-600 underline">
             here
           </a>
           .
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
